52c61b49566badafa8e79e3537e89a187db86e13
Soccoel <suinobr14@gmail.com>
2020-04-01 15:49:55 -0300

    1 file modified

--- index.js
+++ index.js
@@ -20,19 +20,23 @@ import React from 'react';
     render () {
       return (
       <div> 
+      
       <h1>Exemplo com props:</h1>
       <h2> Abaixo vai aparecer as caracteristicas do meu computador:</h2> 
       <h3> Eu tenho um computador da cor: {this.props.cor }</h3> 
       <h3> A memoria do meu computador é de: {this.props.memoria}  </h3> 
       <h3> O processador do meu computador é um: {this.props.processador} </h3>
       <h3> O HD do meu computador é de: {this.props.HD} </h3>
+      
       <h1>Exemplo com states:</h1>
       <h2> Abaixo vai aparecer as caracteristicas do meu computador:</h2> 
       <h3> Eu tenho um computador da cor: {this.state.cor }</h3> 
       <h3> A memoria do meu computador é de: {this.state.memoria}  </h3> 
       <h3> O processador do meu computador é um: {this.state.processador} </h3>
       <h3> O HD do meu computador é de: {this.state.HD} </h3>
+      
       <button type='buton' onClick={this.mudacomputador}>Mudar Computador</button>
+
       </div>
       );
     }
@@ -40,9 +44,11 @@ import React from 'react';
 function Monitor (props) { 
   return(
     <div>
+    
     <h2> Abaixo vai aparecer a descriçao do meu monitor: </h2> 
     <h3> As polegadas do monitor sao: {props.polegadas} </h3> 
     <h3> A marca do meu monitor é: {props.marca} </h3>
+   
     </div>
   );
 }
@@ -53,7 +59,7 @@ class App extends Component {
     this.state ={
     name: "React", 
     propmarca : "Dell",
-     proppolegadas : "13pl"
+    proppolegadas : "13pl"
   };
 }
 
@@ -65,9 +71,13 @@ class App extends Component {
  render() {
     return (
       <div>
+        
         <Computador processador = 'Intel Core i5-5200u' memoria = '4gb' HD = '250gb' cor = 'Red'/>
+        
         <Monitor polegadas={this.state.proppolegadas} marca={this.state.propmarca}/>
+       
         <button type='buton' onClick={this.mudarmonitor}>Mudar Monitor</button>
+      
       </div>
     );
   }
